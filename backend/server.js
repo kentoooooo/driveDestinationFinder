6a11fb586a35235fb59238daa5558ccf53a844f4
kentoooooo <astra-kent@g.ecc.u-tokyo.ac.jp>
2024-01-04 15:50:26 +0900

    ポート番号変更

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ app.get('/', (req, res) => {
 });
 
 // 8000番ポートでサーバーを起動
-app.listen(8000, () => {
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
   console.log('Server is running on port 8000');
 });
 
